4db94050b855b1484ff57b50eacec8b09efca665
IanValente <ian.valente@yahoo.com.br>
2023-04-08 18:46:39 -0300

    Configurando Spring Data JPA

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -1,19 +1,29 @@
 package med.vol.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import med.vol.api.medico.DadosCadastroMedico;
+import med.vol.api.medico.Medico;
+import med.vol.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 	
+	@Autowired
+	private MedicoRepository medicoRepository;
+	
 	@PostMapping
-	public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-		System.out.println(dados);
+	@Transactional
+	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+		medicoRepository.save(new Medico(dados));
+		
 	}
 
 }
