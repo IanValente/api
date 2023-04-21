1355dd87c8e9d172ffe79e58c7710f2cd34a1967
IanValente <ian.valente@yahoo.com.br>
2023-04-21 13:43:09 -0300

    Implementação de testes automatizados e configuração de build pra deploy

--- src/main/java/med/vol/api/controller/ConsultaController.java
+++ src/main/java/med/vol/api/controller/ConsultaController.java
@@ -8,13 +8,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.vol.api.domain.consulta.AgendaDeConsultas;
 import med.vol.api.domain.consulta.DadosAgendamentoConsulta;
-import med.vol.api.domain.consulta.DadosDetalhamentoConsulta;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 	
 	@Autowired
--- src/main/java/med/vol/api/controller/PacienteController.java
+++ src/main/java/med/vol/api/controller/PacienteController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.vol.api.domain.paciente.DadosAtualizacaoPaciente;
 import med.vol.api.domain.paciente.DadosCadastroPaciente;
@@ -23,6 +24,7 @@ import med.vol.api.domain.paciente.PacienteRepository;
 
 @RestController
 @RequestMapping("pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteController {
 
 	@Autowired
